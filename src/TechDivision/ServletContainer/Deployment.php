177b4340e5e85dd3fb7de1bcf8a38216da4a4cf6
Johann Zelger <zelger@me.com>
2014-02-08 11:50:36 +0100

    test coding standard failure

--- src/TechDivision/ServletContainer/Deployment.php
+++ src/TechDivision/ServletContainer/Deployment.php
@@ -20,14 +20,12 @@ use TechDivision\ApplicationServer\AbstractDeployment;
  * Class Deployment
  *
  * @category  Appserver
- * @package   TechDivision_ServletContainer
  * @author    Tim Wagner <tw@techdivision.com>
  * @copyright 2013 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      http://www.appserver.io
  */
-class Deployment extends AbstractDeployment
-{
+class Deployment extends AbstractDeployment {
 
     /**
      * Returns an array with available applications.
